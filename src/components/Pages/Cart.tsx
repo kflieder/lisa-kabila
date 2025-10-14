10e474af37ba79f962672f97ed886e7312856457
Kacey <125841074+kflieder@users.noreply.github.com>
2025-10-13 21:07:08 -0600

    Add Firebase integration and config file
    Added Firebase as a project dependency and created a firebase.config.js file for configuration. Updated Cart.tsx to support Firebase integration. This sets up the groundwork for using Firebase services in the application.

--- src/components/Pages/Cart.tsx
+++ src/components/Pages/Cart.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { db } from '../../firebase.config'
 
 function Cart() {
+  console.log(db);
   return (
     <div className="fixed top-20 right-0 w-[80vw] h-[8vh] flex items-center justify-center border bg-white">
       Cart page
