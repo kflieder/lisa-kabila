ff72c27fb590d422a0950b6eafd59ba668f1dadc
Kacey <125841074+kflieder@users.noreply.github.com>
2025-10-16 19:29:19 -0600

    Update page.tsx

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,6 +1,8 @@
 'use client'
-import SuccessPage from '@/components/Pages/Success'
+import dynamic from 'next/dynamic'
 import React from 'react'
+
+const SuccessPage = dynamic(() => import('@/components/Pages/Success'), { ssr: false })
 
 function page() {
   return (
